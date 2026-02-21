8451b73580fac9c7f05cdf992f690d3313141024
Kasumi Kari <142251010+KasumiKari@users.noreply.github.com>
2026-02-21 16:26:05 +0100

    Add division program with zero-check

---
+++ Week3_Assignment_Q2_var1.py
@@ -0,0 +1,7 @@
+a = float(input("Let's divide! \nEnter the numerator: "))
+b = float(input("Enter the denominator: "))
+
+if b == 0:
+    print("My dear sunshine, division by zero causes the error ^.^")
+else:
+    print(a/b)
