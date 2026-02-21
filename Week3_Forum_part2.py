0ab2538287304ee254cff4fe8df12cce3d3e450b
Kasumi Kari <142251010+KasumiKari@users.noreply.github.com>
2026-02-21 16:22:16 +0100

    Add sleep hours evaluation script

---
+++ Week3_Forum_part2.py
@@ -0,0 +1,11 @@
+hours_slept = float(input("How many hours did you sleep today? "))
+
+
+if hours_slept >= 8:
+    if hours_slept == 8:
+        print("Congrats! You've got a good rest at night :)")
+    else:
+        print("Cat-level sleeping purrrfectly reached ^.^ ")
+else:
+    print('Hard days happen. I believe in you.')
+
